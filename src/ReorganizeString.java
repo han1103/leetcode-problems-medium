2b87177fab83a6ca4792aa55510486c006de1287
Lei Han <leihan_99@yahoo.com>
2019-11-10 11:22:53 -0500

    ...

---
+++ src/ReorganizeString.java
@@ -0,0 +1,19 @@
+
+public class ReorganizeString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String reorganizeString(String S) {
+		
+		if(S==null || S.length()<3)
+			return S;
+		
+		for(char c : S.) {
+			
+		}
+        return S;        
+    }
+}
